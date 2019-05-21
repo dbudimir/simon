50221e709fea897223ef2c479145d5c3437eb107
dbudimir <dav.budimir@gmail.com>
2019-05-21 11:27:02 -0400

    Created a timed loop to show the existing inputs.

--- main.js
+++ main.js
@@ -1,27 +1,47 @@
-class Step {
-    constructor(color) {
-        this.color = color;
-    }
-}
+const colors = ['red', 'blue', 'yellow', 'green'];
 
-class Order {
-    constructor() {
-        this.steps = [];
-        const color = ['red', 'blue', 'yellow', 'green'];
-
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < color.length; j++) {
-                const step = new Step(color[j]);
-                this.steps.push(step);
-            }
-        }
-    }
-}
-
-const routine = new Order().steps;
 const compSteps = [];
+const userSteps = [];
+const gameSquare = document.querySelector('.game-box');
+// const blue = document.querySelector('#blue');
+// const yellow = document.querySelector('#yellow');
+// const green = document.querySelector('#green');
+const submit = document.querySelector('#start-game');
 
 function draw() {
-    const randomIndex = Math.floor(Math.random() * routine.length);
-    compSteps.push(routine[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    compSteps.push(colors[randomIndex]);
+    console.log(compSteps);
+}
+
+// Ads the first color to the routine when the user clicks start.
+submit.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('The game has started.');
+    draw();
+});
+
+// Ads the users input to an array when they click a certain color.
+gameSquare.addEventListener('click', e => {
+    e.preventDefault();
+    const userSquare = event.target;
+    userSteps.push(userSquare.id);
+    console.log(userSteps);
+});
+
+// This function will loop through the existing function and draw a new card.
+
+let score = 0; //  set your counter to 1
+
+function compShow() {
+    //  create a loop function
+    setTimeout(function() {
+        //  call a 3s setTimeout when the loop is called
+        console.log(compSteps[score]); //  your code here
+        score++; //  increment the counter
+        if (score < compSteps.length) {
+            //  if the counter < 10, call the loop function
+            compShow(); //  ..  again which will trigger another
+        } //  ..  setTimeout()
+    }, 2000);
 }
