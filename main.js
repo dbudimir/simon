a4739179a578a486792b8d2eeca8a45ee86c0cb7
dbudimir <dav.budimir@gmail.com>
2019-05-22 13:46:58 -0400

    Logic is 100% done. Added score.

--- main.js
+++ main.js
@@ -36,36 +36,43 @@ gameSquare.addEventListener('click', e => {
     e.preventDefault();
     const userSquare = e.target;
     const currentGameColor = compSteps[currentUserStep];
+    // Flash the selected square.
     userSquare.setAttribute('class', 'square flash');
     setTimeout(function() {
         userSquare.setAttribute('class', 'square');
     }, 500);
+    console.log(`User clicked on ${userSquare.id}`);
+    console.log(`Current game color ${currentGameColor}`);
+    console.log(`User step ${currentUserStep}`);
+    // Clear and reset the timer
     clearTimeout(initial);
     startTimer();
     // Incrament current user step by 1
     currentUserStep += 1;
-    if (currentUserStep === compSteps.length) {
+    // debugger;
+    if (currentGameColor !== userSquare.id) {
+        clearTimeout(initial);
+        document.querySelector('.game-over').setAttribute('style', 'display:block;');
+    } else if (currentUserStep === compSteps.length) {
+        document.querySelector('.score').innerHTML = compSteps.length;
         clearTimeout(initial);
         draw();
         setTimeout(function() {
             compShow();
         }, 2000);
-    } else if (currentGameColor !== userSquare.id) {
-        clearTimeout(initial);
-        document.querySelector('.game-over').setAttribute('style', 'display:block;');
     }
 });
 
 const compShow = function() {
     compSteps.forEach(function(color, index) {
-        clearTimeout(initial);
         setTimeout(function() {
             document.querySelector(`#${color}`).setAttribute('class', 'square flash');
             setTimeout(function() {
                 document.querySelector(`#${color}`).setAttribute('class', 'square');
             }, 1000);
-        }, 1500 * index);
+            clearTimeout(initial);
+            startTimer();
+        }, 1250 * index);
     });
     currentUserStep = 0;
-    startTimer();
 };
