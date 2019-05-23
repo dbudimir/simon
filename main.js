fe572b26c9db70c0d2818fc7de2f9bda7978e617
dbudimir <dav.budimir@gmail.com>
2019-05-23 10:26:07 -0400

    Added interactivity to the game board.

--- main.js
+++ main.js
@@ -127,3 +127,45 @@ window.onload = function prepCats() {
         getData(url);
     }
 };
+
+// ////////////////////////////////////
+// Tilt effect
+
+const lightBox = document.querySelector('.js-tilt-container');
+lightBox.addEventListener('mousemove', e => {
+    document.querySelector('.light').setAttribute(
+        'style',
+        `left: ${e.pageX - 300}px;
+    top: ${e.pageY - 300}px`
+    );
+});
+
+const tiltBox = document.querySelector('.js-tilt-container');
+
+tiltBox.addEventListener('mousemove', e => {
+    const left = tiltBox.offsetLeft;
+    const top = tiltBox.offsetTop;
+    const cursPosX = e.pageX - left;
+    const cursPosY = e.pageY - top;
+    const cursFromCenterX = tiltBox.offsetWidth / 2 - cursPosX;
+    const cursFromCenterY = tiltBox.offsetHeight / 2 - cursPosY;
+    tiltBox.setAttribute(
+        'style',
+        `transform:
+    perspective(500px) rotateX(${cursFromCenterY / 120}deg) rotateY(${-(cursFromCenterX / 120)}deg) translateZ(10px);`
+    );
+
+    const invertedX = Math.sign(cursFromCenterX) > 0 ? -Math.abs(cursFromCenterX) : Math.abs(cursFromCenterX);
+
+    // // Parallax transform on image
+    // tiltBox.classList.add('.js-perspective-neg');
+    // tiltBox.setAttribute(
+    //     'style',
+    //     `transform: translateY(${cursFromCenterY / 10}px) translateX(${-(invertedX / 10)}px) scale(1);`
+    // );
+    tiltBox.classList.remove('.leave');
+});
+
+tiltBox.addEventListener('mouseleave', () => {
+    tiltBox.classList.add('.leave');
+});
