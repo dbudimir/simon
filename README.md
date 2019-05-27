a1d858da1f3a05ee5ca7798001a12371f75d0204
dbudimir <dav.budimir@gmail.com>
2019-05-26 20:40:09 -0400

    Fixed issues in Firefox.

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ How To:
 -  Click start and select a mode.
 -  Simon will indicate which squares you need to click on.
 -  Follow the pattern and pay close attention, the sequence will get longer in each round.
+
+Javascript:
+
+The Javascript behind this game is made up of three categories.
+
+1. Game Logic
+
+2) Cat API
+
+3) Game Box Interactivity.
+
+CSS:
+
+HTML:
--- main.js
+++ main.js
@@ -1,25 +1,28 @@
+// Randcom cat API
 const url = 'https://api.thecatapi.com/v1/images/search';
 const catBuffer = [];
 
+// Sounds
 const redSound = document.querySelector('#red-sound');
 const blueSound = document.querySelector('#blue-sound');
 const yellowSound = document.querySelector('#yellow-sound');
 const greenSound = document.querySelector('#green-sound');
 const startSound = document.querySelector('#green-sound');
 
-let currentSound;
-
+// Computer steps and user steps
 const colors = ['red', 'blue', 'yellow', 'green'];
 const compSteps = [];
-
 let currentUserStep = 0;
 
+// Dom elements for user messages.
 const gameSquare = document.querySelector('.game-box');
 const errorMsg = document.querySelector('#sorry');
 const headerText = document.querySelectorAll('.header-text');
 
+// Normal or cat mode
 let mode = 'normal';
 
+// Timer for user to take an action.
 let initial;
 function startTimer() {
     initial = setTimeout(function() {
@@ -29,6 +32,13 @@ function startTimer() {
     }, 5000);
 }
 
+// Draws a random color and adds it to compSteps array.
+function draw() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    compSteps.push(colors[randomIndex]);
+}
+
+// Produces game mode options when the users slects "Start"
 const startButton = function() {
     document.querySelector('.game-over').setAttribute('style', 'display:block;');
     errorMsg.innerHTML = `<span>Pick a mode before we get started.</span>`;
@@ -45,6 +55,7 @@ const startButton = function() {
     document.querySelector('#cat-mode').addEventListener('click', startCatMode);
 };
 
+// Starts normal mode
 const startGame = function() {
     errorMsg.setAttribute('style', 'display:none;');
     draw();
@@ -53,6 +64,7 @@ const startGame = function() {
     }, 2000);
 };
 
+// Starts cat mode
 const startCatMode = function() {
     errorMsg.setAttribute('style', 'display:none;');
     mode = 'cat';
@@ -63,20 +75,15 @@ const startCatMode = function() {
     }, 2000);
 };
 
-// Draws a random color and adds it to compSteps array.
-function draw() {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    compSteps.push(colors[randomIndex]);
-}
-
 // On click compare the current game color with compSepts at the index of current user step.
 gameSquare.addEventListener('click', e => {
+    // When the user clicks on a quare
     if (e.target.classList.contains('square')) {
         e.preventDefault();
         const userSquare = e.target;
         const currentGameColor = compSteps[currentUserStep];
         // Play sound
-        eval(`${userSquare.id}Sound`).play();
+        this.eval(`${userSquare.id}Sound`).play();
         // Flash the selected square.
         userSquare.setAttribute('class', 'flash');
         setTimeout(function() {
@@ -87,10 +94,13 @@ gameSquare.addEventListener('click', e => {
         startTimer();
         // Incrament current user step by 1
         currentUserStep += 1;
+        // Get a new cat incase of cat mode.
         getData(url);
+        // If this is the first time a user clicks
         if (compSteps.length === 0) {
             clearTimeout(initial);
             startButton();
+            // If the user selects the wrong color
         } else if (currentGameColor !== userSquare.id) {
             clearTimeout(initial);
             errorMsg.setAttribute('style', 'display:block;');
@@ -99,33 +109,37 @@ gameSquare.addEventListener('click', e => {
             document
                 .querySelector('#real-answer')
                 .setAttribute('style', `text-transform: uppercase; color:  ${currentGameColor};`);
+            // If the user has clicked through the sequence in the correct order
         } else if (currentUserStep === compSteps.length) {
             document.querySelector('.calc-score').innerHTML = compSteps.length + 1;
             document.querySelector('.cats-temp').innerHTML = `<img src="${catBuffer[currentUserStep]}" id="random-cat">
                 <img src="${catBuffer[currentUserStep + 1]}" id="random-cat">`;
-            clearTimeout(initial);
-            draw();
             headerText.forEach(function(title) {
                 title.innerText = 'Simon says...';
                 title.classList.remove('appear');
                 void title.offsetWidth;
                 title.classList.add('appear');
             });
+            clearTimeout(initial);
+            draw();
             setTimeout(function() {
                 compShow();
             }, 2000);
         }
+        // When the user clicks the start button
     } else {
         startButton();
     }
 });
 
+// Simon says - Reproduces the pattern for the user
 const compShow = function() {
     compSteps.forEach(function(color, index) {
         setTimeout(function() {
             const currentSquare = document.querySelector(`#${color}`);
             currentSquare.setAttribute('class', 'flash');
-            eval(`${color}Sound`).play();
+            this.eval(`${color}Sound`).play();
+            // If cat mode, show a cat instead of the background color
             if (mode === 'cat') {
                 currentSquare.setAttribute(
                     'style',
@@ -142,6 +156,7 @@ const compShow = function() {
             startTimer();
         }, 1250 * index);
     });
+    // Remind the user when it's their turn to go again
     setTimeout(function() {
         headerText.forEach(function(title) {
             title.innerText = 'Your turn!';
@@ -150,16 +165,19 @@ const compShow = function() {
             title.classList.add('appear');
         });
     }, 1250 * compSteps.length + 1);
+    // Reset the user steps
     currentUserStep = 0;
 };
 
+// Reload the page when the user clicks "Try Again"
 function reload() {
     errorMsg.setAttribute('style', 'display:none;');
-    location.reload();
+    location.reload(true);
 }
 
 // ////////////////////////////////////  // Random Cat API
 
+// Gets a new cat url and pushes it to the cat array
 const getData = function(myUrl) {
     fetch(myUrl, {
         headers: {
@@ -172,13 +190,12 @@ const getData = function(myUrl) {
         })
         .catch(err => {
             alert('error');
-            console.log('something went wrong...', err);
         });
 };
 
 // Preloads two cat images into the buffer.
 document.addEventListener('DOMContentLoaded', () => {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 4; i += 1) {
         getData(url);
     }
     const promise = new Promise(function(resolve) {
@@ -195,6 +212,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // ////////////////////////////////////  // Tilt effect
 
+// Adds a sof light over the gamebox at the cursor
 const lightBox = document.querySelector('.js-tilt-container');
 lightBox.addEventListener('mousemove', e => {
     document.querySelector('.light').setAttribute(
@@ -204,8 +222,8 @@ lightBox.addEventListener('mousemove', e => {
     );
 });
 
+// Tilts the game box based on the users mous position
 const tiltBox = document.querySelector('.js-tilt-container');
-
 tiltBox.addEventListener('mousemove', e => {
     const left = tiltBox.offsetLeft;
     const top = tiltBox.offsetTop;
@@ -224,6 +242,7 @@ tiltBox.addEventListener('mousemove', e => {
     );
 });
 
+// Resets the gamebox when the user mouses away.
 tiltBox.addEventListener('mouseleave', () => {
     tiltBox.setAttribute('style', 'transform: rotateX(0) rotateY(0); -webkit-transform: rotateX(0) rotateY(0) ;');
 });
